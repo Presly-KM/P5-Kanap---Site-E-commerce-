c359dc4d8b104ce5e8f6907d6803e991b617c9ea
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-04-14 15:29:47 +0200

    confirmation.js pushing

---
+++ front/js/confirmation.js
@@ -0,0 +1,21 @@
+const orderId = getOrderId()                                      // On appelle la fonction ecrite plus bas destinée a récupérer le numéro de commande
+displayOrderId(orderId)                                           // On appelle la fonction ecrite plus bas destinée a afficher le numero de commande 
+removeAllCache()                                                  // On appelle la fonction ecrite plus bas destinée a supprimer le cache (les données du localstorage)
+ 
+
+function getOrderId() {                                           // On cherche ici à récuperer l'id de commande (orderId) qui est dans l'url (après le "?") et qui se trouve en paramètre de la requête (ex : ?orderId=1234567890)
+const queryString = window.location.search                        // On va récupèrer la partie de l'url qui contient les paramètres de la requête (après le "?")
+const urlParams = new URLSearchParams(queryString)                // On va créer un objet URLSearchParams à partir de la chaîne de requête (queryString) pour pouvoir manipuler les paramètres de l'url plus facilement
+return urlParams.get("orderId")                                   // Cette fonction va "retourner" (par l'intermediaire de "return") l'id qu'aura trouvé le URLSearchParams
+}
+
+
+function displayOrderId() {                                       // On va maintenant afficher l'id trouvé en html                   
+    const orderIdElement = document.getElementById("orderId")     // On va chercher l'élément html qui a pour id "orderId" et on l'assigne à la variable orderIdElement
+    orderIdElement.textContent = orderId                          // On y incorpore l'id de commande comme dans le html de tel sorteque : "<p>Votre commande n°1234567890</p>"
+}
+ 
+function removeAllCache() {                                       // On vide, supprime les données du local storage une fois la commande validée et le numéro de commande communiqué !
+    const cache = window.localStorage                             //
+    cache.clear()                                                 // 
+}
