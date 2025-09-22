41918e50bee77756ac8b08e94ca1ac8b0f9c32bb
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-09-22 19:12:12 +0200

    retour à la normal

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,20 @@
 //@ts-nocheck
+const InStorage = []                                                                // On crée un tableau vide qui va contenir les articles envoyés par l'utilisateur dans le panier(dans le localStorage) . On l'assigne à la variable InStorage.
+
+retrieveAllItemsFromStorage()                                                       // On appelle la fonction retrieveAllItemsFromStorage pour récupérer tous les articles du panier stockés dans le localStorage.
+
+
 const queryString = window.location.search                                          // Window.location.search est une propriété qui renvoie la partie de l'URL qui suit le "?" et qui contient les paramètres de la requête.(Ex : ?id=a557292fe5814ea2b15c6ef4bd73ed83). Elle retourne la partie chaines de requête (Querystring) de l'URL qui commence par le "?" et qui contient les paramètres de la requête.
 const urlParams = new URLSearchParams(queryString)                                  // URLSearchParams est une interface qui fournit des méthodes pour travailler avec les paramètres d'URL. Elle permet de récupérer les valeurs des paramètres de la requête en utilisant leur nom.
 const id = urlParams.get("id")                                                      // On veut récupérer la valeur de l"id du produit (qui se trouve au sein du querystring) . La constante id va donc ici automatiquement afficher la valeur de l'id ciblée (les inscriptions après le "?") On utilise la méthode get de l'interface URLSearchParams pour récupérer la valeur de l'id ciblé.
+
+
+
 if (id != null) {                                                                   // On crée ici une variable globale pour gérer le problème dans la fonction "saveorder" concernant le localStorage. si l'id existe... NOTE if à supprimer : ne garder que ligne 14 et 15 englober le fetch aussi
     let itemPrice = 0                                                               // On met ici le prix(Itemprice) à 0 par defaut afin qu'il soit modifié par le prix contenu dans l'api au sein de la fonction "handledata(couch)" a la ligne 33 selon la règle de priorité de la variable locale sur la variable globale et qu'ainsi la nouvelle valeur d'"ItemPrice" puisée dans le "price" venant de l'API (handleData) soit reutilisée dans "data" (de la fonction saveOrder) et puisse ainsi être désormais reconnu dans le localStorage (car la valeur du prix avant ce procédé n'etait pas reconnu par le LocalStorage)
+    let imgUrl, altText, articleName                                                // On met ici imgUrl, alText, articleName pour les memes raisons (pas reconnu par le localStorage)
 }
+
 
 
 fetch(`http://localhost:3000/api/products/${id}`)                                   // On fait une deuxième requête au serveur pour récupèrer les données propres à l'id du produit que l'on cible (sur lequel on a cliqué). La valeur de l'id est ici récupérée grâce à l'URLSearchParams. (cf.ligne 8à9)  
@@ -13,12 +23,21 @@ fetch(`http://localhost:3000/api/products/${id}`)                               
 
 
 function handleData(couch) {                                                        // On crée une fonction qui va nous permettre de gérer les données du produit. On lui passe en paramètre les données du produit (couch).
-    const { altTxt, colors, description, imageUrl, name, price } = couch            // On récupère individuellement tous les objets qui sont stockées dans handledata                                                             
-    itemPrice = price                                                               // On modifie la valeur de la variable globale "itemPrice" (cf.ligne 6) par la valeur du prix (price) qui est tirée de l'API. On fait cela pour que le localStorage puisse reconnaitre la valeur du prix et l'inclure dans le panier. cf. ligne 76
-    makeImage(imageUrl, altTxt)                                                     // Après avoir récupéré indivuellment les données, on les passe dans les fonctions makeImage, makeTitle, makePrice, makeDescription et makeColors 
+    // const altTxt = couch.altTxt
+    // const colors = couch.colors    
+    // const description = couch.description                                            
+    // const imageUrl = couch.imageUrl
+    // const name = couch.name
+    // const price = couch.price            
+    const { altTxt, colors, description, imageUrl, name, price } = couch            // On récupère individuellement tous les objets qui sont stockées dans handledata
+    itemPrice = price                                                               // Pour remedier au problème de l'affichage du prix dans le localStorage on recupére ici le prix depuis l'api puis grace à la règle de priorité de la variable locale sur la variable globale le prix de la variable "itemPrice" situé à la ligne 8 est modifié. En effet, en règle générale, dans le cas ou une même variable (ici ItemPrice) est déclaré à la fois en local et en global, la variable locale (au sein d'une fonction) l'emporte sur la variable globale. "ItemPrice" dans sa ligne 8 ne vaut donc plus "0" mais "price".
+    imgUrl = imageUrl                                                               // meme raison que ci-dessus
+    altText = altTxt                                                                // meme raison
+    articleName = name                                                              // meme raison 
+    makeImage(imageUrl, altTxt)                                                     // Après avoir récupéré indivuellment les données, on les passe dans les fonctions makeImage, makeTitle, makePrice, makeCartContent et makeColors 
     makeTitle(name)
     makePrice(price)
-    makeDescription(description)
+    makeCartContent(description)
     makeColors(colors)
 }
 
@@ -48,7 +67,7 @@ function makePrice(price) {                                                     
 
 
 
-function makeDescription(description) {                                             // On ajoute ici la description des pices
+function makeCartContent(description) {                                             // On ajoute ici la description des pices
     const p = document.querySelector("#description")                                // On va chercher l'élément qui va encadrer la génération de la description (le <p id="description">) et on l'assigne à la variable p.
     if (p != null) p.textContent = description                                      // On y incorpore la description du produit comme dans le html de tel sorte que : "<p id="description">Un canapé 2 places en tissu gris clair</p>"
 }
@@ -70,22 +89,65 @@ function makeColors(colors) {
 
 
 const button = document.querySelector("#addToCart")                                 // On met en place le bouton "Ajouter dans le panier"
-if (button != null) {                                                               // On verifie que le bouton existe bien (pour eviter le message d'erreur "object is possibly null") et si il n'existe pas...
-button.addEventListener("click", (e) => {                                           // A la place de "event" on met "handleClick" qui signifie : Quand on clique sur le bouton, applique directement la focntion "handleClick"                                                                           // On crée une fonction qui va nous permettre de gérer le clic sur le bouton "Ajouter dans le panier". On lui passe en paramètre l'événement du clic.
+button.addEventListener("click", handleClick)                                       // A la place de "event" on met "handleClick" qui signifie : Quand on clique sur le bouton, applique directement la focntion "handleClick"
+
+function handleClick() {                                                            // On crée une fonction qui va nous permettre de gérer le clic sur le bouton "Ajouter dans le panier". On lui passe en paramètre l'événement du clic.
     const color = document.querySelector("#colors").value                      
     const quantity = document.querySelector("#quantity").value                      // On récupère la valeur de la couleur et de la quantité sélectionnée par l'utilisateur dans le menu déroulant et le champ de saisie. On utilise la propriété value pour récupérer la valeur sélectionnée.
                                                                                
-    if (color == null || color === "" || quantity == null || quantity ==0) {                                                                 // Puis il va nous rediriger vers le panier  / Appel de la fonction "redirectToCart" créée ci-dessous 
-        alert("Please select a color and a quantity")                               // Si la couleur n'est pas sélectionnée ou si la quantité est nulle, on affiche une alerte pour demander à l'utilisateur de sélectionner une couleur et une quantité.
-    }
-                                                                                 // On crée une clé unique pour chaque article en combinant l'id du produit et la couleur sélectionnée. Cela permet de stocker plusieurs articles similaires mais avec des couleurs différentes dans le localStorage. Or cela était impossible avant. On utilise la syntaxe `${}` pour interpoler les valeurs de id et color dans la chaîne de caractères. l'id est puisé dans la ligne 4 (const id = urlParams.get("id")) et la couleur est puisée dans la ligne 76 (const color = document.querySelector("#colors").value)
+    if (isOrderInvalid(color, quantity)) return                                     // ...Si la couleur et la quantité est incorrectement entrée alors la fonction IsOrderinvalid s'applique ce qui va entrainer l'affichage d'un message d'erreur et l'arrêt du processus par l'intermediaire de "return"...
+    saveOrder(color, quantity)                                                      // ...Sinon dés lors qu'on clique dans "Ajouter dans le Panier" il va sauvegarder toutes les données dans le localStorage grace à l'appel de la fonction "saveOrder" créee ci-dessous 
+    alert("The item was added to your cart ! 👍");
+    redirectToCart()                                                                // Puis il va nous rediriger vers le panier  / Appel de la fonction "redirectToCart" créée ci-dessous 
+}
+
+
+
+function saveOrder(color, quantity) {                                               // Après avoir cliqué sur "Ajouter dans le panier" on va sauvegarder dans le localStorage les informations de l'article à acheter (l'id, la couleur, la quantité, le prix etc.) 
+    const key = `${id}-${color}`                                                    // On crée une clé unique pour chaque article en combinant l'id du produit et la couleur sélectionnée. Cela permet de stocker plusieurs articles similaires mais avec des couleurs différentes dans le localStorage. Or cela était impossible avant. On utilise la syntaxe `${}` pour interpoler les valeurs de id et color dans la chaîne de caractères. l'id est puisé dans la ligne 4 (const id = urlParams.get("id")) et la couleur est puisée dans la ligne 76 (const color = document.querySelector("#colors").value)
     const data = {                                                                  // On crée un objet data qui va contenir toutes les informations de l'article à stocker dans le localStorage. On utilise la syntaxe d'objet littéral pour créer l'objet.
         id: id,
         color: color,
         quantity: Number(quantity),                                                 // Quantity on en fait un Number. Pour eviter que dans la console les chiffres s'affichent en string. Exemple: "1" au lieu de 1.
         price: itemPrice,                                                           // Avant = "price: price" mais on rencontrait des problemes pour que le localStorage parvienne a lire la valeur du prix. On reorganise alors le code en haut de la page de telle manière que la valeur du prix soit reconnu par le localStorage. Itemprice vaut ici "price" qui est tiré de l'API. cf. ligne 25
+        imageUrl: imgUrl,
+        altTxt: altText,
+        name: articleName
     }
-    localStorage.setItem(id, JSON.stringify(data))                                                             // LocalStorage prend ici l'identifiant (ou la clé) et la valeur à stocker. De telle manière que : localStorage.setItem("identifiant", "valeur").   JSON.stringify --> LocalStorage n'est pas capable de storer des objets, on est obligé de les sérialiser, autrement dit, de les transformer en string. 
-    window.location.href = "cart.html"
-})
+    localStorage.setItem(key, JSON.stringify(data))                                                             // LocalStorage prend ici l'identifiant (ou la clé) et la valeur à stocker. De telle manière que : localStorage.setItem("identifiant", "valeur").   JSON.stringify --> LocalStorage n'est pas capable de storer des objets, on est obligé de les sérialiser, autrement dit, de les transformer en string. 
+    
+    const itemToUpdate = InStorage.find((item) => item.id === id && item.color === color);                      // On cherche si l'article existe déjà dans le panier (dans le localStorage) en utilisant la méthode find. On compare l'id et la couleur de l'article pour trouver une correspondance. Si l'article existe, on le met à jour.
+    if (itemToUpdate != null) {                                                                                 // On vérifie si l'article à modifier est bien présent dans le panier/dans le localStorage.
+        increaseOfQuantity = itemToUpdate.quantity + Number(quantity)                                           // Si l'article est déjà présent dans le panier/localstorage, on additionne la quantité déjà présente dans le panier avec la nouvelle quantité sélectionnée par l'utilisateur. On utilise Number pour convertir la valeur de quantity en nombre.
+        itemToUpdate.quantity = increaseOfQuantity                                                              // On met à jour la quantité de l'article dans l'objet data.                                                                                                          
+        localStorage.setItem(key, JSON.stringify(itemToUpdate))                                                 // On met à jour les données de l'article dans le localStorage en utilisant la clé unique créée précédemment. On utilise la méthode setItem pour mettre à jour les données et JSON.stringify pour les transformer en chaîne de caractères.                                                                                                                // On va chercher l'élément qui va encadrer la génération de la quantité (le <input id="quantity">) et on l'assigne à la variable inputItemQuantity.
+        alert(`This item is already in the cart. The quantity has been increased : ${increaseOfQuantity}`)      // On affiche une alerte pour informer l'utilisateur que l'article est déjà présent dans le panier et que la quantité a été mise à jour. 
+    }
 }
+
+
+
+function retrieveAllItemsFromStorage() {                                              // Concernant les articles pour lesquels on a cliqué sur "Ajouter dans le panier", on veut récupérer ces articles stokés dans le localStorage pour après les faire apparaitre dans le panier (cart.html).
+    const numberOfItems = localStorage.length                                         // On veut savoir combien d'articles sont présents dans le localStorage. On utilise localStorage.length. On l'assigne à la variable numberOfItems.
+    for (let i = 0; i < numberOfItems; i++) {                                         // On parcours et récupère tous les articles présents dans le localStorage grace à une boucle for.
+        const item = localStorage.getItem(localStorage.key(i)) || ""                  // On récupère chaque article du localStorage en utilisant la méthode getItem de l'interface Storage. On utilise localStorage.key(i) pour récupérer la clé de chaque article. Si l'article n'existe pas, on lui assigne une chaîne vide "".
+        const itemObject = JSON.parse(item)                                           // On transforme la chaîne JSON en objet JavaScript en utilisant JSON.parse. Cela nous permet de récupérer les données de l'article sous forme d'objet.
+        InStorage.push(itemObject)                                                    // On met l'objet récupéré dans le "InStorage" (panier) ci-dessus. 
+    }
+}
+
+
+
+function isOrderInvalid(color, quantity) {                                          // Gestion de la validité du choix de la couleur et de la quantité du produit à acheter
+    if (color == null || color === "" || quantity == null || quantity == 0) {
+        alert("Please select a color and a quantity ! ⛔⚠")
+        return true                                                                 // Cette fonction renvoie "true" si une seule de ces conditions est remplie
+    }
+}
+
+
+
+function redirectToCart() {                                                         // Gestion de la redirection vers le panier après l'ajout au panier.
+    window.location.href = "cart.html"
+}
+
